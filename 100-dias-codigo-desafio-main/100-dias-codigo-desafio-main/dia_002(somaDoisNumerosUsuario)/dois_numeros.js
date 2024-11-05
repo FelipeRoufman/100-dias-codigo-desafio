226bc235ac0f2b9df6fe71a383d5f19aa5ff68f1
Felipe_Roufman <161071252+FelipeRoufman@users.noreply.github.com>
2024-11-05 15:14:39 -0300

    Delete 100-dias-codigo-desafio-main/100-dias-codigo-desafio-main directory

--- 100-dias-codigo-desafio-main/100-dias-codigo-desafio-main/dia_002(somaDoisNumerosUsuario)/dois_numeros.js
+++
@@ -1,8 +0,0 @@
-const prompt = require('prompt-sync')()
-
-let n1 = Number(prompt('Me diga o primeiro numero: '))
-let n2 = Number(prompt('Me diga o segundo numero: '))
-
-n1 += n2
-
-console.log(`O resutado da soma deles dois é ${n1}`)
--- 100-dias-codigo-desafio-main/100-dias-codigo-desafio-main/dia_14(ThemaBlack)/main.js
+++
@@ -1,8 +0,0 @@
-const themeSwitcher = document.querySelector('.theme-switcher input');
-
-themeSwitcher.addEventListener('change', () => {
-    document.body.setAttribute(
-        'data-theme',
-        themeSwitcher.checked ? 'dark' : 'light'
-    );
-});
