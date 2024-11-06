38f11a0513ece3420aa6ebed51470e316e9ed05b
Felipe_Roufman <161071252+FelipeRoufman@users.noreply.github.com>
2024-11-06 09:55:29 -0300

    dia 17 feito

--- 100-dias-codigo-desafio-main/dia_003(veriaficarPar)/numeros_primos.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')()
-
-let n = Number(prompt('Me diga qual numero voce quer verificar se é par ou impa: '))
-
-if (n % 2 === 0) {
-    console.log(`${n} é um número par.`);
-  } else {
-    console.log(`${n} é um número ímpar.`);
-  }
